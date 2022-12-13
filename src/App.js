d4436f2a8464d0a0adfd0904ad39ca28cc7fee73
Zachary Rosenfeld <zachary_rosenfeld1+cs1300@brown.edu>
2022-12-13 15:39:39 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import "./App.css";
-import Accordion from "react-bootstrap/Accordion";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import Row from "react-bootstrap/Row";
-import Tab from "react-bootstrap/Tab";
 import Personas from "./Personas";
-import Button from "react-bootstrap/Button";
 
 function App() {
   return (
